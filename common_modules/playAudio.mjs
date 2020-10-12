fc0504f03ccbfb24dc7ff6a44d450753f81ddfce
Lucas Hofmockel-Spanakis <14125393+PieInOblivion@users.noreply.github.com>
2020-10-12 18:19:14 +1100

    updated spotify node dep, ytdl buffer to 8MB

--- common_modules/playAudio.mjs
+++ common_modules/playAudio.mjs
@@ -4,7 +4,8 @@ import { stopBot } from '../bot_modules/stop.mjs';
 import ytdl from 'ytdl-core-discord';
 
 export async function playAudio(serverProperties) {
-	const stream = ytdl(serverProperties.playing, { quality: 'highestaudio' });
+	// 8 MB buffer
+	const stream = ytdl(serverProperties.playing, { filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1<<23 });
 
 	if (!serverProperties.voiceChannel) {
 		serverProperties.voiceChannel = await serverProperties.lastMessage.member.voice.channel.join();

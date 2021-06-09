572fb61a66a70f5db3ffdd2031033a8cea21293c
Lucas Hofmockel-Spanakis <14125393+PieInOblivion@users.noreply.github.com>
2021-06-10 05:03:38 +1000

    removed old module that handled dynamic files

--- common_modules/objToFile.mjs
+++
@@ -1,7 +0,0 @@
-import { writeFile } from 'fs';
-
-export function ObjToFile(loc, obj) {
-    writeFile(`./secret/${loc.split("/").pop()}`, JSON.stringify(obj), function writeJSON(err) {
-		if (err) return console.log(err);
-	});
-}
